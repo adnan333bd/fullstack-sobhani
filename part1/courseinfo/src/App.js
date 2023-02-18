ace690bb276bf4da29432d6910907347a8eb4f5e
golam sobhani chowdhury <golamsobhani.chowdhury@hm.com>
2023-02-18 19:30:31 +0600

    add semicolon

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return <h1>{props.course}</h1>;
-}
+};
 
 const Part = ({ part, exercise }) => {
   return (
@@ -23,13 +23,13 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
 
   return (
     <div>
@@ -47,6 +47,6 @@ const App = () => {
         exercises3={exercises3} />
     </div>
   );
-}
+};
 
-export default App
+export default App;
