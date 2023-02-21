c3f105a1d1d35951a62429cc88553cde425e08f0
golam sobhani chowdhury <golamsobhani.chowdhury@hm.com>
2023-02-21 18:54:05 +0600

    start project for unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+
+  const setToZero = () => setCounter(0);
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+    </div>
+  );
+};
+
+export default App;
